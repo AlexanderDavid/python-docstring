4912e296b4916ea38272571c7c27412a27aa33cb
Alex <alex@alexday.me>
2020-05-17 22:41:28 -0400

    Added more documentation and removed redundant code

--- python-docstring.el
+++ python-docstring.el
@@ -2,40 +2,52 @@
 ;;
 ;; Copyright (C) 2020 Alex Day
 ;;
-;; Author: Alex Day <http://github/alexanderdavid>
+;; Author: Alex Day <http://github/AlexanderDavid>
 ;; Maintainer: Alex Day <alexday135@gmail.com>
 ;; Created: May 11, 2020
 ;; Modified: May 11, 2020
 ;; Version: 0.0.1
 ;; Keywords: Python, Docstring, Google-Style
-;; Homepage: https://github.com/alexexanderdavid/python-docstring.el
+;; Homepage: https://github.com/alexexanderdavid/python-docstring
 ;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
 ;;
+;; To use this file, put something like the following in your ~/.emacs:
+;;
+;; (add-to-list 'load-path /path/to/python-docstring)
+;; (require 'python-docstring)
+;;
+;; And then bind the (python-docstring/generate-docstring) to a certain
+;; key like so:
+;;
+;; (global-set-key (kbd "<f9>") 'python-docstring/generate-docstring)
+;;
+;; This function can be called when the cursor is over a python function
+;; header. It will produce a google-ish style docstring.
+;;
 ;;; To Do:
-;;    - Make indentation match
 ;;    - Find way to prompt user to enter data with tab stuff
 ;;
 ;;; Code:
 
-;; Struct for the argument of a function
-(cl-defstruct arg name type def)
+;; Struct to store information for one argument
+(cl-defstruct python-docstring/--arg name type def)
 
 (defun python-docstring/get-function-header ()
-  "Find the function header at the current cursor position"
+  "Find the function header at the current cursor position."
+
+  ;; Search backwards to the def and then forwards to the :
   (let ((start (search-backward "def"))
-        (end   (search-forward "\)")))
+        (end   (re-search-forward "\).*:")))
+    ;; Replace newlines and whitespace with blanks and return the
+    ;; substring
     (replace-regexp-in-string "\n" ""
       (replace-regexp-in-string "\s*" ""
-        (if (buffer-substring end (+ end 6))
-            (buffer-substring start (search-forward ":"))
-            (buffer-substring start end)
-            ))))
-
-  )
+            (buffer-substring start end))))
+)
 
 (defun python-docstring/get-function-indentation ()
   "Get the indentation of the current function."
@@ -45,85 +57,134 @@
     )
   )
 
-(defun python-docstring/get-function-name (header)
-  "Given a function header return the function's name."
+(defun python-docstring/--get-function-name (header)
+  "Given a function HEADER return the function's name."
+  ;; Match the regex that contains def[ANYTHING](. The ANYTHING
+  ;; should contain the function name
   (string-match "def.*\("
                 header)
 
+  ;; Return the substring that contains the function name
   (substring (match-string 0 header) 4 -1)
 )
 
-(defun python-docstring/get-function-return-type (header)
-  "Get the return type if one exists"
+(defun python-docstring/--get-function-return-type (header)
+  "Get the return type from the HEADER if one exists."
+
+  ;; When the string contains -> return the type
   (when (string-match "->" header)
     (substring header (match-end 0) -1)
     )
-  )
+)
 
-(defun python-docstring/argument-builder (arg-text)
-  "Build an argument struct from a string."
+(defun python-docstring/--argument-builder (arg-text)
+  "Build an argument struct from an ARG-TEXT string."
   (if (string-match ":.*=" arg-text)
       (let ((args (split-string (replace-regexp-in-string "=" ":"
                                  (replace-regexp-in-string " " "" arg-text)) ":")))
-        (make-arg :name (car args) :type (car (cdr args)) :def (car (cdr (cdr args)))))
+        (make-python-docstring/--arg
+         :name (car args)
+         :type (car (cdr args))
+         :def (car (cdr (cdr args)))))
+
     (if (string-match ":" arg-text)
         (let ((args (split-string arg-text ":")))
-          (make-arg :name (car args) :type (car (cdr args))))
+          (make-python-docstring/--arg
+           :name (car args)
+           :type (car (cdr args))))
+
       (if (string-match "=" arg-text)
           (let ((args (split-string arg-text "=")))
-           (make-arg :name (car args) :def (car (cdr args))))
-        (make-arg :name arg-text))))
+            (make-python-docstring/--arg
+             :name (car args)
+             :def (car (cdr args))))
+
+        (make-python-docstring/--arg
+         :name arg-text))))
 )
 
 (defun python-docstring/get-function-args (header)
-  "Given a function header return all args, types, and default values."
+  "Given a function HEADER (from --get-function-header) return all args, types, and default values."
+
+  ;; Match inbetween the parenthesis to get the args
   (string-match "\(.*\)"
                 header)
 
-  (mapcar 'python-docstring/argument-builder
-          (split-string (substring (match-string 0 header) 1 -1) ",\s?"))
+  ;; Define the arguments as the arguments from the header split by commas with optional
+  ;; spaces
+  (let ((arguments (split-string (substring (match-string 0 header) 1 -1) ",\s?")))
+    ;; Apply the argument-builder to each argument string
+    (mapcar 'python-docstring/--argument-builder arguments))
+)
 
+(defun python-docstring/--print-arg (arg indentation)
+  "Print the given ARG for a docstring with a given INDENTATION.
+This will print the comment in the Google docstring style:
+  arg_name (arg_type, optional: arg_default_value)"
+
+  ;; Only print the string if it is not the "self" argument
+  (when (not (string= (python-docstring/--arg-name arg) "self"))
+    ;; Indent
+    (insert indentation)
+    (insert indentation)
+    (insert indentation)
+
+    ;; Insert the argument name
+    (insert (python-docstring/--arg-name arg))
+
+    ;; Check for either a type or a default value
+    (when (or
+            (python-docstring/--arg-def arg)
+            (python-docstring/--arg-type arg))
+
+      ;; Insert the paren for the type/default value
+      (insert " (")
+
+      ;; If there is a type then return the type
+      (if (python-docstring/--arg-type arg)
+          (insert (python-docstring/--arg-type arg)))
+
+      ;; Print the comma if there is both a default argument and
+      ;; a type
+      (if (and
+            (python-docstring/--arg-def arg)
+            (python-docstring/--arg-type arg))
+            (insert ", "))
+
+      ;; If there is a default argument then print that
+      (when (python-docstring/--arg-def arg)
+        (insert "default: ")
+        (insert (python-docstring/--arg-def arg)))
+
+      ;; Close the paren
+      (insert ")"))
+
+    ;; Insert the : and go to the next line
+    (insert ": \n"))
   )
-
 
 (defun python-docstring/generate-docstring ()
   "Generate documentation for function under cursor."
   (interactive)
   (let* ((header (python-docstring/get-function-header))
          (args (python-docstring/get-function-args header))
-         (return-type (python-docstring/get-function-return-type header))
+         (return-type (python-docstring/--get-function-return-type header))
          (indentation (make-string (python-docstring/get-function-indentation) ?\s)))
     (search-forward "\)")
-    (next-line)
+    (forward-line)
     (beginning-of-line)
     (insert "\n")
-    (previous-line)
+    (forward-line -1)
     (insert indentation)
     (insert indentation)
     (insert "\"\"\"\n")
     (insert indentation)
     (insert indentation)
     (insert "Args:\n")
+
+
     (mapc (lambda (arg)
-              (when (not (string= (arg-name arg) "self"))
-                  (insert indentation)
-                  (insert indentation)
-                  (insert indentation)
-                   (insert (arg-name arg))
-                   (when (or (arg-def arg) (arg-type arg))
-                     (insert " (")
-                     (if (arg-type arg)
-                         (insert (replace-regexp-in-string "\s" "" (arg-type arg))))
-                     (if (and (arg-type arg) (arg-def arg))
-                         (insert ", "))
-                     (when (arg-def arg)
-                       (insert "default: ")
-                       (insert (arg-def arg))
-                       )
-                     (insert ")")
-                     )
-                   (insert ": \n")
-                )
+            (python-docstring/--print-arg arg indentation)
               ) args)
 
     (when return-type
@@ -143,7 +204,7 @@
     (insert indentation)
     (insert "\"\"\"\n")
   )
-)
+  )
 
 
 (provide 'docstring)
